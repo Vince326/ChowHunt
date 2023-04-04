0e6dfb34ef0c4715b9f84327611b7c3e60659dd0
Vincent Hunter <jhearst29@gmail.com>
2023-04-03 22:11:16 -0400

    Adding the map to the DetailView Screen

---
+++ ChowHunt/View/RestaurantDetailMapCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantDetailMapCell.swift
+//  ChowHunt
+//
+//  Created by Vincent Hunter on 4/3/23.
+//
+
+import UIKit
+import MapKit
+
+
+class RestaurantDetailMapCell: UITableViewCell {
+    
+    @IBOutlet var mapView: MKMapView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
